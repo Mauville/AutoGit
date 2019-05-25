b24e152e1884e02c4ae8f7d03934b01566b7abf2
Oscar <4991127@outlook.com>
2019-05-24 23:57:08 -0500

    Closing semester commit

--- 04_Init.ps1
+++ 04_Init.ps1
@@ -1,15 +1,27 @@
+Write-Host "You can get help with these commands in the README"
 $name = Read-Host "Please input your github username"
 $email = Read-Host "Please input your github email"
-$repo = Read-Host "Please input the project name from where you dowloaded this file E.g. "Mauville/XXXXXXX""
+$repo = Read-Host "Please input the project ID. E.g. "Mauville/XXXXXXX""
+
+
+$name = $name.Trim()
+$email = $repo.Trim()
+$repo = $repo.Trim()
+
+$altrepo = $repo.Replace("/","-")
 
 git config --global user.name "$name"
 git config --global user.email "$email"
 
 
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
-mkdir $DesktopPath\$repo
-cd $DesktopPath\$repo
 
-git clone https://www.github.com/$repo.git
+mkdir $DesktopPath\$altrepo
+cd $DesktopPath\$altrepo
 
-cd $repo
+$url = "https://www.github.com/"+ $repo + ".git"
+git clone $url
+
+$folder = $repo.Substring($repo.IndexOf("/"),$repo.length)
+
+cd $folder
